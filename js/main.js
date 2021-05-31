180e0cc327aad3644e6185df661fc03e83aac5bb
Papanov0 <Papanov0@yandex.ru>
2021-05-31 20:16:15 +0300

    Revert "Ветка"
    This reverts commit 8f14de80e2a52ee27b6fc6d70a6f6c371327af7d.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
+let money; 
 
-console.log('sdfgsdvd');
+let income;
+
+let addExpenses;
+
+let deposit;
+
+let mission;
+
+let period;
+
+
+
+alert('Врываемся в JS');
+
+console.log('Браузер не пугайся, свои!');
