5ec61de04edac390113d2f30e1205f1857fcc24c
Papanov0 <Papanov0@yandex.ru>
2021-06-04 17:16:53 +0300

    третье задание

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
-let money = 100000; 
+let money = prompt('Ваш месячный доход?', '30000'); 
 
 let income = 'фриланс';
 
-let addExpenses = 'Интернет, Такси, Коммуналка';
+let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'газ, свет, вотда');
 
-let deposit = true;
+let deposit = prompt('Есть ли у вас депозит в банке?', 'false');
+
+let expenses1 = prompt('Введите обязательную статью расходов?', 'газ, свет, вотда');
+
+let expenses2 = prompt('Введите обязательную статью расходов?', 'газ, свет, вотда');
+
+let amount1 = prompt('Во сколько это обойдется?', '10000');
+
+let amount2 = prompt('Во сколько это обойдется?', '15000');
+
+let budgetMonth = money - amount1 - amount2;
+
 
 let mission = 10000000;
 
@@ -22,5 +33,23 @@ console.log('Цель заработать ' + mission + ' рублей');
 
 console.log(addExpenses.toLowerCase().split());
 
-let budgetDay = money / 30;
-console.log(budgetDay);
+let budgetDay = Math.floor(budgetMonth / 30);
+console.log('бюджет на день: ' + budgetDay);
+
+console.log('бюджет на месяц: ' + budgetMonth);
+
+console.log(Math.ceil(mission / budgetMonth) + ' месяцев');
+
+
+if (budgetDay  >= 1200) {
+    console.log('У вас высокий уровень дохода');
+}
+else if (budgetDay >= 600 && budgetDay <1200) {
+    console.log('У вас средний уровень дохода');
+} 
+else if (budgetDay < 600 && budgetDay >= 0) {
+    console.log('К сожалению у вас уровень дохода ниже среднего');
+}
+else {
+    console.log('Что то пошло не так');
+}
