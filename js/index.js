40916eb60d667961558cd38f5a1ee80407530f2b
Papanov0 <Papanov0@yandex.ru>
2021-08-11 22:03:58 +0300

    урок девятый

---
+++ js/index.js
@@ -0,0 +1,71 @@
+'use strict';
+
+let startButton = document.getElementById('start'),
+
+    plusButton = document.getElementsByTagName('button')[0],
+
+    plusButton2 = document.getElementsByTagName('button')[1],
+
+    checkBox = document.querySelector('#deposit-check'),
+
+    expensesAll = document.querySelectorAll('.additional_income-item'),
+
+    budgetMonth = document.getElementsByClassName('budget_month-value'),
+
+    budgetDay = document.getElementsByClassName('budget_day-value'),
+
+    expensesMonth = document.getElementsByClassName('expenses_month-value'),
+
+    additionalIncome = document.getElementsByClassName('additional_income-value'),
+
+    additionaExpenses = document.getElementsByClassName('additional_expenses-value'),
+
+    incomePeriod = document.getElementsByClassName('income_period-value'),
+    
+    targetMonth = document.getElementsByClassName('target_month-value'),
+
+    salaryAmount = document.querySelector('.salary-amount'),
+
+    incomeTitle = document.querySelector('.income-title'),
+
+    incomeAmount = document.querySelector('.income-amount'),
+
+    additionalIncomeItem = document.querySelector('.additional_income-item'),
+
+    expensesTitle = document.querySelector('.expenses-title'),
+
+    expensesAmount = document.querySelector('.expenses-amount'),
+
+    additionalExpensesItem = document.querySelector('.additional_expenses-item'),
+
+    depositCheck = document.querySelector('#deposit-check'),
+
+    periodSelect = document.querySelector('.period-select');
+
+
+    
+
+console.log(startButton);
+
+console.log(plusButton);
+
+console.log(plusButton2);
+
+console.log(checkBox);
+
+console.log(budgetMonth);
+
+console.log(budgetDay);
+
+console.log(expensesMonth);
+
+console.log(additionalIncome);
+
+console.log(additionaExpenses);
+
+console.log(incomePeriod);
+
+console.log(targetMonth);
+
+console.log(expensesAll);
+
